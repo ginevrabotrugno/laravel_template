4f3f44da275896d37170a39e9173dcd60311ffb1
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-10 15:29:32 +0200

    create template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,20 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Layout base | @yield('titlePage')</title>
+    <title>Layout Laravel</title>
 
     {{-- Includiamo gli assets con la direttiva @vite --}}
     @vite('resources/js/app.js')
 </head>
 <body>
 
-    @include('partials.navbar')
-
-    @yield('hero')
-
-    @yield('content')
-
-    @include('partials.footer')
 
 </body>
 </html>
