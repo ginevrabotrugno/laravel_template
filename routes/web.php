5c27950976a0f2f06519d9af0f3bb573aa17eae0
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-12 16:52:45 +0200

    feat: add page controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PageController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/about', [PageController::class, 'about'])->name('about');
 
-Route::get('/contacts', function () {
-    return view('contacts');
-})->name('contacts');
+Route::get('/contacts', [PageController::class, 'contacts'])->name('contacts');
 
 
